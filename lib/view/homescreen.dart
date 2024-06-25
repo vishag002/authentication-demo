4ba8d592c29094861e18ff010b6bba2aae5b1b49
vishag002 <vvishag477@gmail.com>
2024-06-25 21:37:43 +0530

    new commit

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login_authentication_demo/view/acc_delete_screen.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -10,10 +12,14 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  final user = FirebaseAuth.instance.currentUser;
-  final userS = "";
+  final User? user = FirebaseAuth.instance.currentUser;
+
   signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  deletaAcc() async {
+    // await FirebaseAuth.instance.delete;
   }
 
   @override
@@ -30,175 +36,215 @@ class _HomescreenState extends State<Homescreen> {
         centerTitle: true,
       ), */
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 150,
-            width: double.infinity,
-            //color: Colors.amber,
-            child: Stack(
-              children: [
-                Positioned(
-                  left: -110,
-                  top: -70,
-                  child: CircleAvatar(
-                    radius: 110,
-                    backgroundColor: Colors.orange.shade900,
-                  ),
-                ),
-                Positioned(
-                  left: 40,
-                  top: -150,
-                  child: CircleAvatar(
-                    radius: 110,
-                    backgroundColor: Colors.orange.shade900,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Column(
+      body: FutureBuilder<DocumentSnapshot>(
+        future:
+            FirebaseFirestore.instance.collection('users').doc(user!.uid).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return Center(child: Text('User data not found'));
+          }
+
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          String email = user!.email!;
+          String name = data['name'];
+          int age = data['age'];
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                height: 40,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
-                  ),
-                  color: Colors.orange.shade900,
-                ),
-                child: Center(
-                  child: Text(
-                    "Welcome User :",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                height: 150,
+                width: double.infinity,
+                //color: Colors.amber,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: -110,
+                      top: -70,
+                      child: CircleAvatar(
+                        radius: 110,
+                        backgroundColor: Colors.orange.shade900,
+                      ),
                     ),
-                  ),
+                    Positioned(
+                      left: 40,
+                      top: -150,
+                      child: CircleAvatar(
+                        radius: 110,
+                        backgroundColor: Colors.orange.shade900,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    topLeft: Radius.circular(30),
-                  ),
-                  color: Colors.blue.shade900,
-                ),
-                child: Center(
-                  child: StreamBuilder<DocumentSnapshot>(
-                    stream: FirebaseFirestore.instance
-                        .collection('users')
-                        .doc(user!.uid)
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return CircularProgressIndicator();
-                      }
-                      if (snapshot.hasError) {
-                        return Text(
-                          "Error: ${snapshot.error}",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      }
-                      if (!snapshot.hasData || !snapshot.data!.exists) {
-                        return Text(
-                          "User data not found",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      }
-
-                      var userData =
-                          snapshot.data!.data() as Map<String, dynamic>;
-                      var userName = userData['name'];
-
-                      return Text(
-                        "Name: $userName",
+              Column(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      color: Colors.orange.shade900,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Welcome User",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    topLeft: Radius.circular(30),
-                  ),
-                  color: Colors.blue.shade900,
-                ),
-                child: Center(
-                  child: Text(
-                    "email : ${user!.email}",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      color: Colors.blue.shade900,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Name: $name',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.blue.shade900,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Age: $age',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30),
+                      ),
+                      color: Colors.blue.shade900,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "email : $email",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                //color: Colors.amber,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: -160,
+                      child: CircleAvatar(
+                        radius: 120,
+                        backgroundColor: Colors.blue.shade900,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: -220,
+                      right: -5,
+                      child: CircleAvatar(
+                        radius: 150,
+                        backgroundColor: Colors.blue.shade900,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
-          ),
-          Container(
-            height: 200,
-            width: double.infinity,
-            //color: Colors.amber,
-            child: Stack(
+          );
+          ////
+          /* Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Positioned(
-                  right: -160,
-                  child: CircleAvatar(
-                    radius: 120,
-                    backgroundColor: Colors.blue.shade900,
-                  ),
-                ),
-                Positioned(
-                  bottom: -220,
-                  right: -5,
-                  child: CircleAvatar(
-                    radius: 150,
-                    backgroundColor: Colors.blue.shade900,
-                  ),
-                )
+                Text('Email: $email', style: TextStyle(fontSize: 20)),
+                SizedBox(height: 10),
+                Text('Name: $name', style: TextStyle(fontSize: 20)),
+                SizedBox(height: 10),
+                Text('Age: $age', style: TextStyle(fontSize: 20)),
               ],
+            ),
+          ); */
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            shape: CircleBorder(),
+            backgroundColor: Colors.white,
+            onPressed: () {
+              //
+              Get.to(() => AccDeleteScreen());
+            },
+            child: Icon(
+              Icons.delete,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          //
+          SizedBox(width: 10),
+          FloatingActionButton(
+            shape: CircleBorder(),
+            backgroundColor: Colors.white,
+            onPressed: () {
+              signOut();
+            },
+            child: Icon(
+              Icons.logout,
+              color: Colors.black,
+              size: 30,
             ),
           ),
         ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        backgroundColor: Colors.white,
-        onPressed: () {
-          signOut();
-        },
-        child: Icon(
-          Icons.logout,
-          color: Colors.black,
-          size: 30,
-        ),
       ),
     );
   }
